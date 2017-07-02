3e5a026da0e9585d59b1dd9bd445adf45876a8f5
richardsforu <praveen.somireddy@gmail.com>
2017-07-02 14:03:33 +0530

    FS Java

---
+++ JAVA_SE/OOPS-1/src/com/Test.java
@@ -0,0 +1,49 @@
+package com;
+
+class C1{
+	
+	void f2() {
+		
+	}
+}
+
+
+public class Test extends C1{
+	
+	@Override
+	void f2() {
+		
+	}
+	
+
+	void f1() {
+		System.out.println("-- f1 method on "+this.hashCode());
+	}
+	public static void main(String[] args) {
+
+		/*int x=2147483647;
+		System.out.println("int X: "+x);
+		
+		long l=2147483648L;
+		System.out.println("long  L: "+l);*/
+		
+		//float f=764.76f;
+		
+		//byte b=127;
+	
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
